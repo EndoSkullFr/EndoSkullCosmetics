0cf8238b61e1b4982eaad71cdd18290cd7a5b306
BebeDlaStreat <bebe.vandroux@gmail>
2022-02-06 11:43:41 +0100

    add music (first try)

---
+++ src/main/java/fr/endoskull/cosmetics/inventories/MusicInventory.java
@@ -0,0 +1,37 @@
+package fr.endoskull.cosmetics.inventories;
+
+import com.xxmicloxx.NoteBlockAPI.model.Song;
+import com.xxmicloxx.NoteBlockAPI.songplayer.RadioSongPlayer;
+import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
+import fr.endoskull.api.spigot.utils.CustomGui;
+import fr.endoskull.api.spigot.utils.CustomItemStack;
+import fr.endoskull.cosmetics.Main;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+
+public class MusicInventory extends CustomGui {
+    public MusicInventory(Player p) {
+        super(3, "§cEndoSkull §8» §fMusique");
+        File musicFolder = new File(Main.getInstance().getDataFolder(), "musics");
+        if (!musicFolder.exists()) {
+            setItem(0, new CustomItemStack(Material.BARRIER).setName("§cPas de dossier musiques"));
+            return;
+        }
+        if (musicFolder.isDirectory()) {
+            int i = 0;
+            for (String s : musicFolder.list()) {
+                Song song = NBSDecoder.parse(new File(Main.getInstance().getDataFolder(), "musics/" + s));
+                setItem(i, new CustomItemStack(Material.GOLD_RECORD).setName(song.getTitle()).setLore("\n" + song.getDescription()), player -> {
+                    RadioSongPlayer rsp = new RadioSongPlayer(song);
+                    rsp.addPlayer(player);
+                    rsp.setPlaying(true);
+                });
+                i++;
+            }
+        } else {
+            setItem(0, new CustomItemStack(Material.BARRIER).setName("§cPas un dossier"));
+        }
+    }
+}
