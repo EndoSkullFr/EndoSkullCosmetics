4e74d9c673b3bae84c1f6f0e9709c853ccf3450f
BebeDlaStreat <bebe.vandroux@gmail>
2022-02-06 11:51:51 +0100

    stop music

--- src/main/java/fr/endoskull/cosmetics/inventories/MusicInventory.java
+++ src/main/java/fr/endoskull/cosmetics/inventories/MusicInventory.java
@@ -2,6 +2,7 @@ package fr.endoskull.cosmetics.inventories;
 
 import com.xxmicloxx.NoteBlockAPI.model.Song;
 import com.xxmicloxx.NoteBlockAPI.songplayer.RadioSongPlayer;
+import com.xxmicloxx.NoteBlockAPI.songplayer.SongPlayer;
 import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
 import fr.endoskull.api.spigot.utils.CustomGui;
 import fr.endoskull.api.spigot.utils.CustomItemStack;
@@ -10,8 +11,11 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class MusicInventory extends CustomGui {
+    private static HashMap<UUID, RadioSongPlayer> songPlayer = new HashMap<>();
     public MusicInventory(Player p) {
         super(3, "§cEndoSkull §8» §fMusique");
         File musicFolder = new File(Main.getInstance().getDataFolder(), "musics");
@@ -24,9 +28,13 @@ public class MusicInventory extends CustomGui {
             for (String s : musicFolder.list()) {
                 Song song = NBSDecoder.parse(new File(Main.getInstance().getDataFolder(), "musics/" + s));
                 setItem(i, new CustomItemStack(Material.GOLD_RECORD).setName(song.getTitle()).setLore("\n" + song.getDescription()), player -> {
+                    if (songPlayer.containsKey(player.getUniqueId())) {
+                        songPlayer.get(player.getUniqueId()).destroy();
+                    }
                     RadioSongPlayer rsp = new RadioSongPlayer(song);
                     rsp.addPlayer(player);
                     rsp.setPlaying(true);
+                    songPlayer.put(player.getUniqueId(), rsp);
                 });
                 i++;
             }
