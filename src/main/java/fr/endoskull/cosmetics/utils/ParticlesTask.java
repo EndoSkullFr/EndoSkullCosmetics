c759b44ef72f70dea250b9cfb80eb61cc13a55bf
MrKing.exe <sbai.siphayes5@gmail.com>
2022-05-27 18:49:59 +0200

    Fix 'Feu Follets 2.0' particle

--- src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
+++ src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
@@ -132,8 +132,8 @@ public class ParticlesTask extends BukkitRunnable {
 
 
             if (particle == Particles.WHISP2) {
-                Vector v1 = VectorUtils.rotateAroundAxisY(new Vector(MathL.cos(step * Math.PI / 20)*0.7, 1, MathL.sin(step * Math.PI / 20)*0.7), -Math.toRadians(loc.getYaw()));
-                Vector v2 = VectorUtils.rotateAroundAxisY(new Vector(MathL.cos(step * Math.PI / 20)*-0.7, 1, MathL.sin(step * Math.PI / 20)*0.7), -Math.toRadians(loc.getYaw()));
+                Vector v1 = VectorUtils.rotateAroundAxisY(new Vector(MathL.cos(step * Math.PI / 20)*0.7, 1.4, MathL.sin(step * Math.PI / 20)*0.7), -Math.toRadians(loc.getYaw()));
+                Vector v2 = VectorUtils.rotateAroundAxisY(new Vector(MathL.cos(step * Math.PI / 20)*-0.7, 1.4, MathL.sin(step * Math.PI / 20)*0.7), -Math.toRadians(loc.getYaw()));
                 ParticleUtils.sendParticle(EnumParticle.FLAME, loc.clone().add(v1.getX(), v1.getY() + 1, v1.getZ()), player, 1, 0, 0, 0);
                 ParticleUtils.sendParticle(EnumParticle.FLAME, loc.clone().add(v2.getX(), v2.getY() + 1, v2.getZ()), player, 1, 0, 0, 0);
             }
