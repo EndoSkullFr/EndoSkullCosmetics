e09827918e8f02ed2fb89228bd05dbc726966844
MrKing.exe <sbai.siphayes5@gmail.com>
2022-05-27 18:34:10 +0200

    Patch 'Ange_Enfer' particle

--- src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
+++ src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
@@ -117,7 +117,7 @@ public class ParticlesTask extends BukkitRunnable {
             }
 
             if (particle == Particles.ANGEENFER) {
-                    for (double t = 0; t < Math.PI * 2; t += Math.PI / 6) {
+                    for (double t = 0; t < Math.PI * 2; t += Math.PI / 8) {
                         float radius = 0.5f;
                         float angle = 0f;
                         double x = (radius * MathL.sin(t));
