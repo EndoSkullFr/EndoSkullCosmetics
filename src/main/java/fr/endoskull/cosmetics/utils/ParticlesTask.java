a7f4369d91d4caaceaa20a71718f041b5428b177
MrKing.exe <sbai.siphayes5@gmail.com>
2022-05-27 18:30:56 +0200

    Patch 'Ange_Enfer' particle + Patch error in console

--- src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
+++ src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
@@ -116,8 +116,8 @@ public class ParticlesTask extends BukkitRunnable {
                 }
             }
 
-            if (particle == Particles.ANGE_ENFER) {
-                    for (double t = 0; t < Math.PI * 2; t += Math.PI / 16) {
+            if (particle == Particles.ANGEENFER) {
+                    for (double t = 0; t < Math.PI * 2; t += Math.PI / 2) {
                         float radius = 0.5f;
                         float angle = 0f;
                         double x = (radius * MathL.sin(t));
