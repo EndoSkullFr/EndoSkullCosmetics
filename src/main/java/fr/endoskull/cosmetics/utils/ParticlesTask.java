ce5a97c16475d34f873e09f2da7b644a8bffc487
MrKing.exe <sbai.siphayes5@gmail.com>
2022-05-27 18:41:40 +0200

    Fix 'Feu Follets 2.0' particle

--- src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
+++ src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
@@ -131,7 +131,7 @@ public class ParticlesTask extends BukkitRunnable {
             }
 
             if (particle == Particles.WHISP2) {
-                for (double t = 0; t < Math.PI * 2; t += Math.PI / 8) {
+                for (double t = 0; t < Math.PI * 2; t += Math.PI / 2) {
                     float radius = 0.5f;
                     float angle = 0f;
                     double x = (radius * MathL.sin(t));
