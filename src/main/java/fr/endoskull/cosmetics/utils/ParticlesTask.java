4050f03b25c55a0d5289d68780b768fad2f02c7c
MrKing.exe <sbai.siphayes5@gmail.com>
2022-05-28 18:04:31 +0200

    Fix a particle ( "Amoureux" )

--- src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
+++ src/main/java/fr/endoskull/cosmetics/utils/ParticlesTask.java
@@ -145,7 +145,7 @@ public class ParticlesTask extends BukkitRunnable {
             }
 
             if (particle == Particles.AMOUREUX) {
-                for (double t = 0; t < Math.PI * 2; t += Math.PI / 3) {
+                for (double t = 0; t < Math.PI * 2; t += Math.PI / 2 / 2) {
                     float radius = 0.5f;
                     float angle = 0f;
                     double x = (radius * MathL.sin(t));
